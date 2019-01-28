911a266c6e271abaa2786ec2bb029ad04157b336
lol97 <blackmanga80@gmail.com>
2019-01-29 05:23:05 +0700

    add django

---
+++ machine_linux/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/mnt/01D47D4256484F40/Code/Website/SmartHome/machine_linux/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
